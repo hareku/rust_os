b66f56d82b554e30e3333ceeb557c002c4260560
hareku <hareku908@gmail.com>
2020-10-20 11:25:09 +0000

    Add memory management

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -65,10 +66,15 @@ pub fn init() {
     x86_64::instructions::interrupts::enable();
 }
 
-/// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
